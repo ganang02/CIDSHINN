a0bad90c38ea3f4cb679dba4872062be435878ab
ganang02 <91892496+ganang02@users.noreply.github.com>
2021-12-19 15:25:48 +0700

    Update corefunc.h

--- corefunc.h
+++ corefunc.h
@@ -22,10 +22,10 @@
 
 #pragma once
 //Customing GrowID
-string Growid_acc = "capcitest1";
+string Growid_acc = "BotShinnGayy";
 
 //Customing Password
-string Password_acc = "loler1234@";
+string Password_acc = "Shinn@224";
 
 //Customing Gmail ( At least put a random gmail. Dont add @gmail.com )
 string Gmail_acc = "testgmail";
@@ -55,7 +55,7 @@ public:
 	string currentWorld;
 	bool connect = false;
 	int timeFromWorldEnter = 0; // in 10mss...
-	string gameVersion = "3.65";
+	string gameVersion = "3.76";
 	int owner = -1;
 	string ownerUsername;
 	int localX = -1;
